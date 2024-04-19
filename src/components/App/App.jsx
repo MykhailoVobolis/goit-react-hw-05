5b09c1cebe3bd0a043c984387a2d253359ba49bd
MykhailoVobolis <mvobolis@gmail.com>
2024-04-19 14:53:22 +0300

    my first commit - created work spase

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../Navigation/Navigation";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1>Start creat React hw-05</h1>
+      <Navigation />
+    </>
+  );
+}
