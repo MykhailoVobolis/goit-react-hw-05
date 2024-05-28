c0d2247607b84dff29a3d9e434328a477b3cecf4
MykhailoVobolis <mvobolis@gmail.com>
2024-05-28 18:35:55 +0300

    correct slider 1180px

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -28,6 +28,7 @@ export default function Slider({ items }) {
         resistanceRatio={0.1}
         breakpoints={{
           768: { slidesPerView: 4.25, slidesPerGroup: 4 },
+          1180: { slidesPerView: 5.25, slidesPerGroup: 5 },
           1280: { slidesPerView: 5, slidesPerGroup: 5, spaceBetween: 20, speed: 900 },
         }}>
         {newArrayFilms.map((item) => (
