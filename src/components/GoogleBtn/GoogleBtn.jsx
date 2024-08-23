192c5c035d7e0e8a35c10a4e4d2708e3df440cb4
MykhailoVobolis <mvobolis@gmail.com>
2024-08-23 14:46:17 +0300

    fix loader logic on login

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -1,29 +1,22 @@
 import { FcGoogle } from "react-icons/fc";
 
 import { getGoogleUrl } from "../../cinema-server-api.js";
-import Loader from "../Loader/Loader.jsx";
-import { useState } from "react";
 
 import css from "./GoogleBtn.module.css";
 
 export default function GoogleBtn({ type }) {
-  const [loading, setLoading] = useState(false);
-
   const handleGoogleLogin = async () => {
     try {
-      setLoading(true);
       const response = await getGoogleUrl();
       const url = response.data.url; // Получаем URL из ответа
       window.location.href = url; // Перенаправляем пользователя на URL для авторизации Google
     } catch (error) {
-      setLoading(false);
       console.error("Error getting Google auth URL:", error);
     }
   };
 
   return (
     <>
-      {loading && <Loader loading={loading} />}
       {type === "Up" ? (
         <button className={css.googleBtn} onClick={handleGoogleLogin}>
           <FcGoogle className={css.googleIcon} />
