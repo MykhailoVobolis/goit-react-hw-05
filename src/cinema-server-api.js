caedca6eea8df8844c486e252f6a15d653b46390
MykhailoVobolis <mvobolis@gmail.com>
2024-09-04 16:34:41 +0300

    minor refactoring base url

--- src/cinema-server-api.js
+++ src/cinema-server-api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://cinema-hall-app.onrender.com";
+const BASE_URL = "https://cinema-hall-app.onrender.com";
 
 export async function registerUser(newUser) {
-  const response = await axios.post("https://cinema-hall-app.onrender.com/auth/register", newUser, {
+  const response = await axios.post(`${BASE_URL}/auth/register`, newUser, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data;
 }
 
 export async function loginUser(user) {
-  const response = await axios.post("https://cinema-hall-app.onrender.com/auth/login", user, {
+  const response = await axios.post(`${BASE_URL}/auth/login`, user, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data;
@@ -18,7 +18,7 @@ export async function loginUser(user) {
 
 export async function logoutUser() {
   const response = await axios.post(
-    "https://cinema-hall-app.onrender.com/auth/logout",
+    `${BASE_URL}/auth/logout`,
     {},
     {
       withCredentials: true, // Важливо для відправки куків
@@ -29,7 +29,7 @@ export async function logoutUser() {
 
 export async function refreshUser() {
   const response = await axios.post(
-    "https://cinema-hall-app.onrender.com/auth/refresh",
+    `${BASE_URL}/auth/refresh`,
     {},
     {
       withCredentials: true, // Важливо для відправки куків
@@ -39,7 +39,7 @@ export async function refreshUser() {
 }
 
 export async function getGoogleUrl() {
-  const response = await axios.get("https://cinema-hall-app.onrender.com/auth/get-oauth-url", {
+  const response = await axios.get(`${BASE_URL}/auth/get-oauth-url`, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data;
@@ -47,7 +47,7 @@ export async function getGoogleUrl() {
 
 export async function confirmGoogleOAuth(code) {
   const response = await axios.post(
-    "https://cinema-hall-app.onrender.com/auth/confirm-oauth",
+    `${BASE_URL}/auth/confirm-oauth`,
     { code },
     {
       withCredentials: true, // Важливо для відправки куків
@@ -57,28 +57,28 @@ export async function confirmGoogleOAuth(code) {
 }
 
 export async function addMovie(movie) {
-  const response = await axios.post("https://cinema-hall-app.onrender.com/movies", movie, {
+  const response = await axios.post(`${BASE_URL}/movies`, movie, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data;
 }
 
 export async function delMovie(id) {
-  const response = await axios.delete(`https://cinema-hall-app.onrender.com/movies/${id}`, {
+  const response = await axios.delete(`${BASE_URL}/movies/${id}`, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data;
 }
 
 export async function getFavoriteMovies() {
-  const response = await axios.get("https://cinema-hall-app.onrender.com/movies", {
+  const response = await axios.get(`${BASE_URL}/movies`, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data.data;
 }
 
 export async function getMovieById(id) {
-  const response = await axios.get(`https://cinema-hall-app.onrender.com/movies/${id}`, {
+  const response = await axios.get(`${BASE_URL}/movies/${id}`, {
     withCredentials: true, // Важливо для відправки куків
   });
   return response.data.data;
