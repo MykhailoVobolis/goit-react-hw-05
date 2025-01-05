240a6ee42aabbfc1d77d6ac01359e27eb56c8ab5
MykhailoVobolis <mvobolis@gmail.com>
2025-01-05 22:59:15 +0200

    added animation for title

--- src/components/TvAdvertising/TvAdvertising.jsx
+++ src/components/TvAdvertising/TvAdvertising.jsx
@@ -4,7 +4,7 @@ import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Loader from "../../components/Loader/Loader";
 
 import { getNowPlaying } from "../../tmdb-api";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import css from "./TvAdvertising.module.css";
 
@@ -13,6 +13,9 @@ export default function TvAdvertising() {
   const [page, setPage] = useState(1);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const sectionRef = useRef(null);
 
   useEffect(() => {
     async function fetchMovies() {
@@ -31,11 +34,35 @@ export default function TvAdvertising() {
     fetchMovies();
   }, []);
 
+  // Анімація заголовку секції
+  useEffect(() => {
+    const currentRef = sectionRef.current;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.25 } // Секція має бути видима на 25%
+    );
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []);
+
   return (
-    <section className={css.tvAdvertising}>
+    <section className={css.tvAdvertising} ref={sectionRef}>
       {loading && <Loader loading={loading} />}
       <div className={css.container}>
-        <div className={css.titleContainer}>
+        <div className={`${css.titleContainer} ${isVisible ? css.visible : ""}`}>
           <h2 className={css.title}>Насолоджуйся українським дубляжем</h2>
         </div>
         <div className={css.tvMacupContainer}>
