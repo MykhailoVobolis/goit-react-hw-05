0d4e683c4e12afef7fe5cd143af0b1b7eb5537cd
MykhailoVobolis <mvobolis@gmail.com>
2025-03-12 01:17:00 +0200

    added styles for genre panel

--- src/components/GenresForMoviesBar/GenresForMoviesBar.jsx
+++ src/components/GenresForMoviesBar/GenresForMoviesBar.jsx
@@ -1,11 +1,11 @@
-import GenresList from "../GenresList/GenresList.jsx";
+import GenreButton from "../GenreButton/GenreButton.jsx";
 
 import css from "./GenresForMoviesBar.module.css";
 
 export default function GenresForMoviesBar({ genres }) {
   return (
     <div className={css.wrapper}>
-      <GenresList genres={genres} />
+      <GenreButton genres={genres} />
     </div>
   );
 }
