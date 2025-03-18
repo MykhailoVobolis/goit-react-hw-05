2ea3f7d9821203128c61b7e3b3c97adb25120229
MykhailoVobolis <mvobolis@gmail.com>
2025-03-19 01:48:07 +0200

    added breadcrumbs

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,20 @@
 import Footer from "../Footer/Footer";
+import AppBar from "../AppBar/AppBar.jsx";
+import BreadcrumbsNav from "../BreadcrumbsNav/BreadcrumbsNav.jsx";
 
 import { Suspense } from "react";
+import { useMedia } from "react-use";
 
 import css from "./Layout.module.css";
-import AppBar from "../AppBar/AppBar.jsx";
 
 export default function Layout({ children }) {
+  const isWide = useMedia("(min-width: 1280px)");
+
   return (
     <div className={css.pageContainer}>
       <AppBar />
       <main className={css.mainContainer}>
+        {isWide && <BreadcrumbsNav />}
         <Suspense fallback={<div></div>}>{children}</Suspense>
       </main>
       <Footer />
