35a4dd85a0cafb0cd414d489c774beca232245b6
MykhailoVobolis <mvobolis@gmail.com>
2025-04-01 00:29:39 +0300

    added react-query and created providers

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { UserProvider } from "./userContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "modern-normalize";
 import App from "./components/App/App";
+import Providers from "./components/providers.jsx";
 import "./index.css";
 
 import "@fontsource/roboto/300.css";
@@ -13,10 +14,12 @@ import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <Providers>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </Providers>
   </React.StrictMode>
 );
